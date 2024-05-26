a945cfb3f4c46de82cf8167922b12bc44562303f
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2024-05-26 22:28:04 +0900

    settingsのlayoutを修正

--- app/dashboard/settings/layout.tsx
+++ app/dashboard/settings/layout.tsx
@@ -1,14 +1,11 @@
 export default function SettingsLayout({
-  children, // will be a page or nested layout
+  children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <section>
-      {/* Include shared UI here e.g. a header or sidebar */}
-      <nav></nav>
-
+    <>
       {children}
-    </section>
+    </>
   )
 }
